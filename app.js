de8a03ef1b3ed23c452d6f5ca022aded95ede008
Marcos Rivas R @vidamrr <marcosrivasr@gmail.com>
2019-09-15 16:33:29 -0500

    Nuevo archivo app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+/*
+  app.js
+  
+  Esta aplcación imprime los nombre de los colaboradores que se meten al repositorio en Github y abren un pull request.
+*/
+const colaboradores = [
+    {nombre: "Marcos Rivas", twitter: "@vidamrr"},
+    {nombre: "Marcos Rivas", twitter: "@vidamrr"},
+    {nombre: "Marcos Rivas", twitter: "@vidamrr"},
+    {nombre: "Marcos Rivas", twitter: "@vidamrr"},
+    {nombre: "Marcos Rivas", twitter: "@vidamrr"},
+    {nombre: "Marcos Rivas", twitter: "@vidamrr"},
+    {nombre: "Marcos Rivas", twitter: "@vidamrr"},
+    {nombre: "Marcos Rivas", twitter: "@vidamrr"},
+    {nombre: "Marcos Rivas", twitter: "@vidamrr"},
+    {nombre: "Marcos Rivas", twitter: "@vidamrr"}
+];
+
+console.log('Esta es la lista de colaboradores en este proyecto');
+colaboradores.forEach(colaborador =>{
+    console.log(`${colaborador.nombre} - ${colaborador.twitter}`);
+});
