f5e5e02af623409d543f57aa2039d605d642d4d1
willymateo <67135809+willymateo@users.noreply.github.com>
2020-06-29 10:57:49 -0500

    Fix: Agregación de mi cuenta de colaborador

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ const colaboradores = [
     {nombre: "Victor Israel", twitter: "@victor-israel"},
     {nombre: "Fabrik Hernandez", twitter: "@FabrikHernandez"},
     {nombre: "Jennifer Cordón", twitter: "@jenny_cordon"},
-    {nombre: "Javier Francisco", twitter: "@naranjas"}
+    {nombre: "Javier Francisco", twitter: "@naranjas"},
+    {nombre: "Willy Mateo", twitter: "@willymateo"}
 ];
 
 console.log('Esta es la lista de colaboradores en este proyecto');
