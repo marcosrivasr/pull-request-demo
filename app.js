df234f9c83eaa9426dae82bbcff73e5654fa2e82
Denilson L. Salas Guzman <denilsonsalguz@gmail.com>
2020-12-23 18:04:26 -0500

    agregando nombre

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ const colaboradores = [
     {nombre: "Emilio Rafael", instagram: "@emilio4585"},
     {nombre: "Fausto Montes", twitter: "@30Fausto"},
     {nombre: "Jorge Cruz", twitter:"@IDon'tHave"},
-    {nombre: "DLSG", twitter:"@"}
+    {nombre: "Denilson SG", twitter:"@"}
 ];
 
 console.log('Esta es la lista de colaboradores en este proyecto');
