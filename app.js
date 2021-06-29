9facc5fbc31ae8a541939a4478ec0bd0052c3322
German Fernandez <germanfernandez0306@gmail.com>
2021-06-29 10:10:41 -0300

    fixed nombre

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ const colaboradores = [
     {nombre: "Emilio Rafael", instagram: "@emilio4585"},
     {nombre: "Fausto Montes", twitter: "@30Fausto"},
     {nombre: "Jorge Cruz", twitter:"@IDon'tHave"},
-    {nombre: "German", twitter: "@germancito"},
+    {nombre: "Germancito", twitter: "@germancito"},
 ];
 
 console.log('Esta es la lista de colaboradores en este proyecto');
